9bc0ebeb20cfad5c8bbf4ffdd73bdba0e7757fc0
Chizzy99 <schesworth1@aol.com>
2024-10-30 16:40:25 +0000

    Projects section added(empty), Javascript added for navbar scroll & click to section

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
+// * Change navbar scroll
+window.addEventListener('scroll', function () {
+	const navbar = document.querySelector('.navbar');
 
+	if (this.window.scrollY > 0) {
+		navbar.classList.add('navbar-scroll');
+	} else {
+		navbar.classList.remove('navbar-scroll');
+	}
+});
+
+// * Click to scroll
+document.querySelectorAll('.scroll-btn').forEach((button) => {
+	button.addEventListener('click', function (e) {
+		e.preventDefault();
+		const targetId = this.getAttribute('target');
+		const targetElement = document.getElementById(targetId);
+
+		if (targetElement) {
+			targetElement.scrollIntoView({ behavior: 'smooth' });
+		} else {
+			window.location.href = `index.html#${targetId}`;
+		}
+	});
+});
